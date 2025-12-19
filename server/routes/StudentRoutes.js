bd1e06dc16231cbb70856c42b587bacc232c37af
ShahadatGithub <shahadat.offl@gmail.com>
2025-12-19 12:02:30 +0530

    fixed server

--- server/routes/StudentRoutes.js
+++ server/routes/StudentRoutes.js
@@ -1,14 +1,29 @@
 import express from "express";
 import { excelUpload } from "../config/multer.js";
-import { createAdmission, deleteStudent, getAllStudents, getStudentById, massAdmission, SearchStudentsByName, updateStudent } from "../controller/studentController.js";
-import { createResult, deleteResult, getAllResults, getSpecificResult, massResults, updateResult } from "../controller/resultController.js";
+
+import {
+    createAdmission, deleteStudent,
+    getAllStudents, getStudentById,
+    massAdmission, SearchStudentsByName,
+    updateStudent
+}
+
+    from "../controller/studentController.js";
+
+import {
+    createResult, deleteResult,
+    getAllResults, getSpecificResult,
+    massResults, updateResult
+}
+    from "../controller/resultController.js";
+
 import { adminAuthMiddleware } from "../middleware/adminAuth.js";
 
 
 const studentRouter = express.Router();
 
 /* ================= STUDENTS ================= */
-studentRouter.get("/list",adminAuthMiddleware, getAllStudents); 
+studentRouter.get("/list", adminAuthMiddleware, getAllStudents);
 studentRouter.get("/single/:id", getStudentById);
 studentRouter.put("/update/:id", updateStudent);
 studentRouter.delete("/delete/:id", deleteStudent);
@@ -23,12 +38,12 @@ studentRouter.post("/admission/mass", adminAuthMiddleware, excelUpload.single("f
 
 // 1. Static/Specific POST routes first
 studentRouter.post("/result/fetch", getSpecificResult); // Public search for student result
-studentRouter.post("/result/mass",adminAuthMiddleware, excelUpload.single("file"), massResults); // Admin mass upload
-studentRouter.post("/result",adminAuthMiddleware, createResult); // Admin single create
+studentRouter.post("/result/mass", adminAuthMiddleware, excelUpload.single("file"), massResults); // Admin mass upload
+studentRouter.post("/result", adminAuthMiddleware, createResult); // Admin single create
 
 // 2. Collection GET routes
-studentRouter.get("/results",adminAuthMiddleware, getAllResults); // Admin fetch all results
-studentRouter.put("/result/:id",adminAuthMiddleware, updateResult);
-studentRouter.delete("/result/:id",adminAuthMiddleware, deleteResult);
+studentRouter.get("/results", adminAuthMiddleware, getAllResults); // Admin fetch all results
+studentRouter.put("/result/:id", adminAuthMiddleware, updateResult);
+studentRouter.delete("/result/:id", adminAuthMiddleware, deleteResult);
 
 export default studentRouter;
