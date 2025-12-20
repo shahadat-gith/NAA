97604769c4b28dba34e024b9ad165d3b5648d558
ShahadatGithub <shahadat.offl@gmail.com>
2025-12-20 11:34:53 +0530

    updated browserrouter

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
 import { AdminContextProvider } from './context/AdminContext.jsx'
 import { TeacherContextProvider } from './context/TeacherContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <AdminContextProvider>
       <TeacherContextProvider>
         <AppContextProvider>
@@ -15,5 +15,5 @@ createRoot(document.getElementById('root')).render(
         </AppContextProvider>
       </TeacherContextProvider>
     </AdminContextProvider>
-  </HashRouter>
+  </BrowserRouter>
 )
