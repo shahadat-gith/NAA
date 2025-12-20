44bdba43a782f08f85a78ef2a438dbd13cae0a4c
ShahadatGithub <shahadat.offl@gmail.com>
2025-12-20 11:23:59 +0530

    added browser router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { FeeContextProvider } from './context/FeeContext.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
