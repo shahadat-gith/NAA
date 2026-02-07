17ae2e74fbf82c1294f98717143706b04ba9a98b
ShahadatGithub <shahadat.offl@gmail.com>
2026-02-07 16:41:01 +0530

    updated search image format

--- client/src/pages/Student/Pages/Search/StudentSearch.jsx
+++ client/src/pages/Student/Pages/Search/StudentSearch.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../Styles/StudentSearch.css";
-import BG_IMAGE from "/search.jpg";
+import BG_IMAGE from "/search.webp";
 import axios from "axios";
 import { AppContext } from "../../../../context/AppContext";
 
